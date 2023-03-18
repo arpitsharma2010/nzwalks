5b4bd7f8a56f34d5a4364b7d443e86695d24542d
arpeet2010 <arpeet.sharma.1998@gmail.com>
2023-03-18 18:41:03 +0530

    EndOfSection2

---
+++ NZWalks/NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double length { get; set; }
+
+        public Guid RegionId { get; set; }
+
+        public Guid WalkDifficultyId { get; set; }
+
+        public Region Region { get; set; }
+
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
